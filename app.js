78a36e3579654cf0c4c90d57a1b0318419eae6d1
mohammadanang <m.anangnur@gmail.com>
2019-08-06 16:17:49 +0700

    add addition endpoint

--- app.js
+++ app.js
@@ -19,6 +19,25 @@ app.post('/', (req, res) => {
     return res.send(`The email value is ${email}`)
 })
 
+app.post('/tambah', (req, res) => {
+    var a, b, c
+    /**
+     * Data type of variable a & b must be number, not string
+     */
+    a = parseInt(req.body.a)
+    b = parseInt(req.body.b)
+    c = a + b
+
+    /**
+     * Check data type of all variable
+     */
+    console.log(typeof a)
+    console.log(typeof b)
+    console.log(typeof c)
+    
+    return res.send(`The result value is ${c}`)
+})
+
 app.listen(3300, () => {
     console.log(`Example app listening on port 3300`)
 })
