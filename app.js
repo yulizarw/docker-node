6608fe14f5c440bf509a706a4d7654eda19cf3e0
mohammadanang <m.anangnur@gmail.com>
2019-08-06 11:29:46 +0700

    add post method example

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send(`Hello World!`))
+app.get('/', (req, res) => {
+    var x, y, z
+    x = 5
+    y = 3
+    z = x + y
+
+    return res.send(`The value of z is ${z}`)
+})
+
+app.post('/', (req, res) => {
+    let email = req.body.email
+    
+    return res.send(`The email value is ${email}`)
+})
 
 app.listen(3300, () => {
     console.log(`Example app listening on port 3300`)
