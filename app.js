32615272ecd8cae10c0c080fbe175856128e790f
mohammadanang <m.anangnur@gmail.com>
2019-08-06 11:25:19 +0700

    update nodemon library

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express()
 
 app.get('/', (req, res) => res.send(`Hello World!`))
 
-app.listen(3000, () => {
-    console.log(`Example app listening on port 3000`)
+app.listen(3300, () => {
+    console.log(`Example app listening on port 3300`)
 })
