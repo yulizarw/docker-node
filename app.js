5792f989883d9ae5c75fa54f29078855e91a1bf4
mohammadanang <m.anangnur@gmail.com>
2019-08-06 19:25:01 +0700

    add new routes called

--- app.js
+++ app.js
@@ -1,42 +1,36 @@
 const express = require('express')
 const app = express()
 
+/**
+ * List of imported routes
+ */
+const array_routes = require('./routes/array')
+
 // for parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    var x, y, z
-    x = 5
-    y = 3
-    z = x + y
-
-    return res.send(`The value of z is ${z}`)
-})
-
-app.post('/', (req, res) => {
-    let email = req.body.email
-    
-    return res.send(`The email value is ${email}`)
+    return res.send(`Welcome to the beginning of nothingness`)
 })
 
 app.post('/tambah', (req, res) => {
     var a, b, c
-    /**
-     * Data type of variable a & b must be number, not string
-     */
+    // Data type of variable a & b must be number, not string
     a = parseInt(req.body.a)
     b = parseInt(req.body.b)
     c = a + b
 
-    /**
-     * Check data type of all variable
-     */
     console.log(typeof a)
     console.log(typeof b)
     console.log(typeof c)
     
     return res.send(`The result value is ${c}`)
 })
+
+/**
+ * Set all routes
+ */
+app.use("/array", array_routes)
 
 app.listen(3300, () => {
     console.log(`Example app listening on port 3300`)
