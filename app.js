58ac44da500151757d5043db6a240df6702a567d
mohammadanang <m.anangnur@gmail.com>
2019-08-07 15:03:11 +0700

    add new array function

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const array_lib = require("./lib/array")
+const object_lib = require("./lib/object")
 
 /**
  * List of imported routes
@@ -25,6 +27,47 @@ app.post('/tambah', (req, res) => {
     console.log(typeof c)
     
     return res.send(`The result value is ${c}`)
+})
+
+app.get('/example-1', (req, res) => {
+    let data = ["Red", "Blue", "Yellow", "Green"]
+    let manipulation = array_lib(data)
+
+    return res.send(manipulation)
+})
+
+app.get('/example-2', (req, res) => {
+    let data = {
+        firstname: "John",
+        lastname: "Doe",
+        email: "john.doe@mail.com",
+        gender: "men"
+    }
+    let result = object_lib.manipulate(data)
+
+    return res.send(result)
+})
+
+app.get("/example-3", (req, res) => {
+    let input = ["Black"]
+    let data = ["Red", "Blue", "Yellow", "Green"]
+    console.log("before ", data)
+
+    let result = object_lib.merge(data = data, input = input)
+    console.log("after ", result)
+
+    return res.send(result)
+})
+
+app.get("/example-4", (req, res) => {
+    let empty_data = []
+    console.log("before ", empty_data)
+
+    let color = "Red"
+    object_lib.add(empty_data, color)
+    console.log("after ", empty_data)
+
+    return res.send(empty_data)
 })
 
 /**
