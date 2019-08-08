a658303ac22d3cfaad1336ef58abc080a4e10d99
mohammadanang <m.anangnur@gmail.com>
2019-08-08 09:50:02 +0700

    add array sort function

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 const express = require('express')
 const app = express()
-const array_lib = require("./lib/array")
-const object_lib = require("./lib/object")
-
-/**
- * List of imported routes
- */
-const array_routes = require('./routes/array')
+const array_sort = require("./lib/array-sort")
 
 // for parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
@@ -15,65 +9,25 @@ app.get('/', (req, res) => {
     return res.send(`Welcome to the beginning of nothingness`)
 })
 
-app.post('/tambah', (req, res) => {
-    var a, b, c
-    // Data type of variable a & b must be number, not string
-    a = parseInt(req.body.a)
-    b = parseInt(req.body.b)
-    c = a + b
+app.get("/task1", (req, res) => {
+    let colors = ["Red", "Blue", "Yellow", "Green"]
+    console.log("Before ", colors)
 
-    console.log(typeof a)
-    console.log(typeof b)
-    console.log(typeof c)
-    
-    return res.send(`The result value is ${c}`)
-})
-
-app.get('/example-1', (req, res) => {
-    let data = ["Red", "Blue", "Yellow", "Green"]
-    let manipulation = array_lib(data)
-
-    return res.send(manipulation)
-})
-
-app.get('/example-2', (req, res) => {
-    let data = {
-        firstname: "John",
-        lastname: "Doe",
-        email: "john.doe@mail.com",
-        gender: "men"
-    }
-    let result = object_lib.manipulate(data)
+    let result = array_sort.ascending_sort(colors)
+    console.log("After ", result)
 
     return res.send(result)
 })
 
-app.get("/example-3", (req, res) => {
-    let input = ["Black"]
-    let data = ["Red", "Blue", "Yellow", "Green"]
-    console.log("before ", data)
+app.get("/task2", (req, res) => {
+    let colors = ["Red", "Blue", "Yellow", "Green"]
+    console.log("Before ", colors)
 
-    let result = object_lib.merge(data = data, input = input)
-    console.log("after ", result)
+    let result = array_sort.descending_sort(colors)
+    console.log("After ", result)
 
     return res.send(result)
 })
-
-app.get("/example-4", (req, res) => {
-    let empty_data = []
-    console.log("before ", empty_data)
-
-    let color = "Red"
-    object_lib.add(empty_data, color)
-    console.log("after ", empty_data)
-
-    return res.send(empty_data)
-})
-
-/**
- * Set all routes
- */
-app.use("/array", array_routes)
 
 app.listen(3300, () => {
     console.log(`Example app listening on port 3300`)
