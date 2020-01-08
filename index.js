ee98109140e78495795211fdc06bd0c7e12bea48
Yulizar Widiatama <yulizarwidiatama@gmail.com>
2020-01-08 13:41:36 +0700

    day 1

--- index.js
+++ index.js
@@ -35,12 +35,29 @@ app.post ('/kelilinglingkaran', (req, res)=>{
     return res.send(`keliling lingkaran kamu adalah ${keliling}`)
 })
 
+app.post ('/regresi', (req, res)=>{
+    //regresi
+    var y, a, b, x, n, regresi
+    // n = parseInt(req.body.n)
+    x = parseInt(req.body.x)
+    y = parseInt(req.body.y)
+    a = parseInt(req.body.a)
+    b = parseInt(req.body.b)
+    // a = (y*x^2)-(x)*(x*y)/n*(x^2)-x^2
+    // b = n(x*y)-(x*y)/n*(x^2)-X^2
+    regresi = a+b*x
 
+    return res.send(`regresi x dan y adalah ${regresi}`)
+})
 
+app.get ('/abc', (req,res)=>{
+    var a, b, c
+    a = 1
+    b = 2
+    c = a+b
 
-
-
-
+    return res.send(`c bernilai ${c}`)
+})
 
 app.listen(3300, () => {
     console.log(`Example app listening on port 3300`)
